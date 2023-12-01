5c0ecbb7ee0a11bbbc2bf24c518e450904b75e3c
James Sample <james.e.sample@gmail.com>
2023-12-01 15:45:46 +0000

    Visulaise input data

--- teotil3/vis.py
+++ teotil3/vis.py
@@ -1,3 +1,6 @@
+import json
+
+import altair as alt
 import contextily as cx
 import folium
 import geopandas as gpd
@@ -8,6 +11,8 @@ import numpy as np
 import pandas as pd
 from folium import IFrame
 from sqlalchemy import text
+
+from . import io
 
 
 def plot_network(
@@ -326,5 +331,173 @@ def point_sources_map(year, eng, loc_type="outlet"):
         feature_group.add_to(m)
 
     folium.LayerControl().add_to(m)
+
+    return m
+
+
+def _validate_input(model_input_file, regine_list, year, par):
+    """Helper function to validate input to 'input_data_summary_map'."""
+    if not isinstance(model_input_file, str):
+        raise TypeError("'model_input_file' must be a valid file path.")
+    if not isinstance(regine_list, list):
+        raise TypeError("'regine_list' must be a list.")
+    if not isinstance(year, int):
+        raise TypeError("'year' must be an integer.")
+    valid_pars = ["totn", "din", "ton", "totp", "tdp", "tpp", "toc", "ss"]
+    if par not in valid_pars:
+        raise ValueError(f"'par' must be one of {valid_pars}.")
+
+    if len(regine_list) > 200:
+        print("WARNING: 'regine_list' is large. Plotting may be slow.")
+
+
+def _get_data(model_input_file, regine_list, year, eng, par):
+    """Helper function to get data for 'input_data_summary_map'."""
+    # Read input file
+    df = pd.read_csv(model_input_file)
+    df = df.query("regine in @regine_list")
+
+    # Get regine polygons
+    reg_gdf = io.get_regine_geodataframe(eng, year)[["regine", "geometry"]]
+    reg_gdf = reg_gdf.query("regine in @regine_list")
+    reg_gdf = reg_gdf.merge(df, how="left", on="regine")
+    reg_gdf = reg_gdf.to_crs("epsg:4326")
+
+    # Get point data
+    sql = text(
+        "SELECT a.site_id, "
+        "  a.name, "
+        "  a.sector, "
+        "  a.type, "
+        "  a.outlet_geom AS geometry, "
+        "  CONCAT_WS('_', d.name, d.unit) AS par_name, "
+        "  (b.value * c.factor) AS value "
+        "FROM teotil3.point_source_locations a, "
+        "  teotil3.point_source_values b, "
+        "  teotil3.input_output_param_conversion c, "
+        "  teotil3.output_param_definitions d "
+        "WHERE b.in_par_id = c.in_par_id "
+        "  AND c.out_par_id = d.out_par_id "
+        "  AND a.site_id = b.site_id "
+        "  AND a.year = :year "
+        "  AND b.year = :year "
+        "  AND d.name = :par"
+    )
+    pt_gdf = gpd.read_postgis(
+        sql, eng, geom_col="geometry", params={"year": year, "par": par.upper()}
+    )
+    pt_gdf = pt_gdf.to_crs("epsg:4326")
+    pt_gdf = gpd.sjoin(
+        pt_gdf, reg_gdf[["regine", "geometry"]], how="inner", predicate="within"
+    )
+
+    return reg_gdf, pt_gdf
+
+
+def _create_map(reg_gdf, pt_gdf, par):
+    """Helper function tjhat creates the Folium map for 'input_data_summary_map'."""
+    # Define a color mapping for the 'sector' column
+    sector_colors = {
+        "Industry": "red",
+        "Large wastewater": "blue",
+        "Small wastewater": "yellow",
+        "Aquaculture": "green",
+    }
+
+    # Create a new Folium map
+    minx, miny, maxx, maxy = reg_gdf.geometry.total_bounds
+    center = [(miny + maxy) / 2, (minx + maxx) / 2]
+    m = folium.Map(location=center)
+
+    # Plot regine polys
+    for _, row in reg_gdf.iterrows():
+        # Create Altair bar chart for par
+        plot_vars = [col for col in reg_gdf.columns if par in col]
+        plot_vars.remove(f"trans_{par}")
+        chart_data = pd.DataFrame(
+            {"Variable": plot_vars, "Value": [row[plot_var] for plot_var in plot_vars]}
+        )
+        chart_data["Variable"] = chart_data["Variable"].str.split(
+            "_", n=1, expand=True
+        )[0]
+        chart = (
+            alt.Chart(chart_data)
+            .mark_bar()
+            .encode(
+                x=alt.X(
+                    "Variable:O",
+                    axis=alt.Axis(labelAngle=45, labelFontSize=12, title=None),
+                ),
+                y=alt.Y("Value:Q", axis=alt.Axis(labelFontSize=12, title=None)),
+                tooltip=alt.Tooltip("Value:Q"),
+            )
+            .properties(
+                title=f"{par.upper()} for regine {row['regine']} (kg)",
+                width=300,
+                height=150,
+            )
+        )
+        chart_json = json.loads(chart.to_json())
+        popup = folium.Popup(max_width=500).add_child(
+            folium.VegaLite(chart_json, width=350, height=200)
+        )
+        folium.GeoJson(row["geometry"], popup=popup).add_to(m)
+
+    # Plot point sources
+    for _, row in pt_gdf.iterrows():
+        table_data = row[["site_id", "name", "sector", "type", "regine"]]
+        new_row = pd.Series({f"{par.upper()} (kg)": row["value"]})
+        table_data = pd.concat([table_data, new_row])
+        html_table = table_data.to_frame().to_html(header=False)
+        popup = folium.Popup(html_table, max_width=500)
+        color = sector_colors.get(row["sector"], "gray")
+        folium.CircleMarker(
+            location=[row["geometry"].y, row["geometry"].x],
+            radius=5,
+            color=color,
+            fill=True,
+            fill_color=color,
+            popup=popup,
+        ).add_to(m)
+
+    m.fit_bounds([[miny, minx], [maxy, maxx]])
+
+    return m
+
+
+def input_data_summary_map(model_input_file, regine_list, year, eng, par):
+    """Create a Leaflet map using Folium to visualise TEOTIL3 input data. The map includes:
+
+        1. Regine polygons from 'regine_list' overlaid on an OpenStreetMap base. Clicking on any
+           regine will display a popup showing a barchart of 'local' inputs to the regine for
+           'par' in 'year', split by source
+        2. Points showing the outlet locations within the regines of interest that discharge 'par'
+           during 'year' (i.e. locations of wastewater, industry and aquculture outlets). Clicking
+           on a point will display a table showing site details
+
+    NOTE: This function is not designed to generate maps for very large areas. It is recommended
+    to keep the length of 'regine_list' in the low hundreds or fewer. Larger datasets will be slow
+    to display and may crash your browser.
+
+    Args
+        model_input_file: Str. Path to TEOTIL3 input file for 'year'
+        regine_list: List of str. List of regine codes to plot
+        year: Int. Year of interest
+        eng: Obj. Active database connection object connected to the TEOTIL3 database
+        par: Str. Parameter of interest. One of
+            ["totn", "din", "ton", "totp", "tdp", "tpp", "toc", "ss"]
+
+    Returns
+        Folium map object.
+
+    Raises
+        TypeError if 'model_input_file' is not a str.
+        TypeError if 'regine_list' is not a list.
+        TypeError if 'year' is not an int.
+        ValueError if 'par' is not in ["totn", "din", "ton", "totp", "tdp", "tpp", "toc", "ss"]
+    """
+    _validate_input(model_input_file, regine_list, year, par)
+    reg_gdf, pt_gdf = _get_data(model_input_file, regine_list, year, eng, par)
+    m = _create_map(reg_gdf, pt_gdf, par)
 
     return m
